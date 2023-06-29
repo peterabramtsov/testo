3e7da3d78be26f528d5db2b3fc17aba435c2c12d
peterabramtsov <yapiramidovich@gmail.com>
2023-06-29 23:51:24 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+import math
+
+a = 0
+a = a + 10
+v = 1
+b = 10
+print(math.cos(1))
